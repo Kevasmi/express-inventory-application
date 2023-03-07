96dd7dba3f2f9df3d52ad30519a75aa926066566
Richard Lasco <lasco9@gmail.com>
2023-03-07 16:09:07 -0600

    Created user route handler

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,18 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+const user_controller = require('../controllers/userController');
+
+/// AUTHENTICATION ROUTES ///
+
+router.get('/sign-up', user_controller.sign_up_get);
+
+router.post('/sign-up', user_controller.sign_up_post);
+
+router.get('/log-in', user_controller.log_in_get);
+
+router.post('/log-in', user_controller.log_in_post);
+
+router.get('/log-out', user_controller.log_out);
 
 module.exports = router;
