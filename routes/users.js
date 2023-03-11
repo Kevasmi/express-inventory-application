b247f08eef217096e9e4a8ffa2ac7019cdb4f705
Richard Lasco <lasco9@gmail.com>
2023-03-10 22:13:31 -0600

    Fixed incorrect routing

--- routes/users.js
+++ routes/users.js
@@ -12,6 +12,6 @@ router.post('/sign-up', user_controller.sign_up_post);
 /// LOG-IN ///
 router.get('/log-in', user_controller.log_in_get);
 router.post('/log-in', user_controller.log_in_post);
-router.get('/log-out', user_controller.log_out);
+router.get('/sign-out', user_controller.sign_out);
 
 module.exports = router;
