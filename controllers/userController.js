43f91b79f8a7d99201fc7babac5530933cea3d56
Richard Lasco <lasco9@gmail.com>
2023-03-10 18:54:10 -0600

    Created post handler for sign-up form

--- controllers/userController.js
+++ controllers/userController.js
@@ -10,22 +10,37 @@ exports.sign_up_get = (req, res, next) => {
   });
 };
 
-exports.sign_up_post = (req, res, next) => {
-  bcrypt.hash(req.body.password, 10, (err, hashedPassword) => {
-    if (err) {
+exports.sign_up_post = [
+  body('username').trim().isLength({ min: 1 }).escape(),
+  body('password').trim().isLength({ min: 1 }).escape(),
+
+  async (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      console.log('Error!');
+      return res.render('sign_up_form', { title: 'Sign Up' });
+    }
+    try {
+      const isUserInDB = await User.find({ username: req.body.username });
+      if (isUserInDB.length > 0)
+        return res.render('sign_up_form', {
+          title: 'Sign Up',
+          error: 'User already exists',
+        });
+      bcrypt.hash(req.body.password, 10, (err, hashedPassword) => {
+        if (err) return next(err);
+        const user = new User({
+          username: req.body.username,
+          password: hashedPassword,
+          admin: req.body.admin,
+          avatar: req.body.avatar,
+        }).save((err) => (err ? next(err) : res.redirect('/')));
+      });
+    } catch {
       return next(err);
     }
-    const user = new User({
-      username: req.body.username,
-      password: hashedPassword,
-    }).save((err) => {
-      if (err) {
-        return next(err);
-      }
-      res.redirect('/');
-    });
-  });
-};
+  },
+];
 
 exports.log_in_get = (req, res, next) => {
   res.render('log_in', {
